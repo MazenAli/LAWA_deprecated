3198fe3881b62aa38f91fb78a4888d1e6cce3703
Kristina Steih <kristina.steih@uni-ulm.de>
2010-06-29 19:28:21 +0200

    Changes in Test program

--- validate/periodic/fwt.cc
+++ validate/periodic/fwt.cc
@@ -9,13 +9,13 @@ main()
 {
     Basis<double,Primal,Periodic,CDF> basis(3,5);
     Basis<double,Dual,Periodic,CDF> basis_(3,5);
-    DenseVectorT x(8,0),y;
-    x = 1,2,3,4,5,2,1,1;
-
+    DenseVectorT x(2,1),y;
+   // x = 1,2,3,4,5,2,1,1;
+    x = 1,0;
     cout << x << endl;
-    fwt(x,basis_,2,y);
+    fwt(x,basis_,0,y);
     cout << y << endl;
-    ifwt(y,basis,2,x);
+    ifwt(y,basis,0,x);
     cout << x << endl;
     
     return 0;
