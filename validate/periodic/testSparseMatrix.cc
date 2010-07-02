4f04da23b25e33d9e24271c1043aec0671d38e10
Kristina Steih <kristina.steih@uni-ulm.de>
2010-07-02 15:46:21 +0200

    Test Program

--- validate/periodic/testSparseMatrix.cc
+++ validate/periodic/testSparseMatrix.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <lawa/lawa.h>
+#include <extensions/flens/flens.h>
 
 using namespace std;
 using namespace lawa;
@@ -10,13 +11,14 @@ typedef flens::DenseVector<flens::Array<T> > DenseVectorT;
 
 int main(){
 
-	DenseVectorT x(8);
-	x = 1,1,1,1,1,1,1,1;
+	DenseVectorT x(4);
+	x = 1,1,1,1;
 	
-	SparseMatrixT A(8,8);
+	SparseMatrixT A(4,4);
 	A(1,1) = 1;
-	A(1,5) = 1;
+	A(2,2) = 1;
 	A(3,4) = 1;
+    A(4,1) = 1;
 	A.finalize();
 	
 	DenseVectorT y;
