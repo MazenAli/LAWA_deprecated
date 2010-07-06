d6574029e992d32e3afe05e8e96bd0a496850ac1
Kristina Steih <kristina.steih@uni-ulm.de>
2010-07-06 11:52:54 +0200

    .h - File Update

--- lawa/box/box.h
+++ lawa/box/box.h
@@ -1,5 +1,6 @@
 #include <lawa/box/boxproblem.h>
 #include <lawa/box/helmholtzoperator.h>
+#include <lawa/box/scalfactorpreconditioner.h>
 #include <lawa/box/separablefunction.h>
 #include <lawa/box/separablerhs.h>
 #include <lawa/box/tensorbasis.h>
