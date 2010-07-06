e63d8ef3fbb048313bdda8e3f78ccf19d830720d
Kristina Steih <kristina.steih@uni-ulm.de>
2010-07-06 17:26:13 +0200

    Evaluate: for 2d Problems

---
+++ lawa/box/evaluate.h
@@ -0,0 +1,16 @@
+#ifndef LAWA_BOX_EVALUATE_H
+#define LAWA_BOX_EVALUATE_H 1
+
+namespace lawa {
+
+template <typename X, typename Basis>
+typename X::ElementType
+evaluate(const Basis &basis, const int J_x, const int J_y, const flens::DenseVector<X> &coeffs,
+         const typename X::ElementType x, const typename X::ElementType y, const int deriv);
+
+} // namespace lawa
+
+#include <lawa/box/evaluate.tcc>
+
+
+#endif // LAWA_BOX_EVALUATE_H
