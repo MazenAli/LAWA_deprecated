b6faf73570f2f1f46cebc9a467195fb914adca63
Kristina Steih <kristina.steih@uni-ulm.de>
2010-07-06 09:41:49 +0200

    .h - Update
    added
     - SeparableFunction 
     - SeparableRHS

--- lawa/box/box.h
+++ lawa/box/box.h
@@ -1,3 +1,5 @@
 #include <lawa/box/boxproblem.h>
 #include <lawa/box/helmholtzoperator.h>
+#include <lawa/box/separablefunction.h>
+#include <lawa/box/separablerhs.h>
 #include <lawa/box/tensorbasis.h>
