60c0e76071e9aa376cafebb65901e25a1151cfa6
Alexander Stippler <alexander.stippler@uni-ulm.de>
2010-08-12 19:09:25 +0200

    added Basis for "R". RefinementMatrix is just a stub. No multiplication so far.

--- lawa/realline/dual/dual.h
+++ lawa/realline/dual/dual.h
@@ -17,5 +17,7 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#include <lawa/realline/dual/basis.h>
 #include <lawa/realline/dual/bspline.h>
+#include <lawa/realline/dual/mra.h>
 #include <lawa/realline/dual/wavelet.h>
--- lawa/realline/primal/primal.h
+++ lawa/realline/primal/primal.h
@@ -17,5 +17,7 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#include <lawa/realline/primal/basis.h>
 #include <lawa/realline/primal/bspline.h>
+#include <lawa/realline/primal/mra.h>
 #include <lawa/realline/primal/wavelet.h>
