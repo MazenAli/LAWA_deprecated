5c990d2bdf5f5905a760c4c660b9f99e77a0a676
Kristina Steih <kristina.steih@uni-ulm.de>
2010-08-16 11:58:55 +0200

    new directory: operators
    First operators: helmholtzoperator1d
    Conflicts:
    
    	lawa/operators/helmholtzoperator1d.h

---
+++ lawa/operators/helmholtzoperator1d.h
@@ -0,0 +1,50 @@
+#ifndef LAWA_PARABOLIC_HELMHOLTZOPERATOR1D_H
+#define LAWA_PARABOLIC_HELMHOLTZOPERATOR1D_H 1
+
+
+#include <lawa/integrals.h>
+#include <lawa/enum.h>
+
+namespace lawa {
+    
+template <typename T, typename Basis>
+class HelmholtzOperator1D{
+    
+    private:
+        
+        const Basis& basis;
+        const T c;
+        
+        typedef typename Basis::BSplineType PrimalSpline;
+        typedef typename Basis::WaveletType PrimalWavelet;
+        
+        PrimalSpline phi, d_phi;
+        PrimalWavelet psi, d_psi;
+        
+        Integral<T, Gauss, PrimalSpline, PrimalSpline> integral_sfsf, dd_integral_sfsf;
+        Integral<T, Gauss, PrimalSpline, PrimalWavelet> integral_sfw, dd_integral_sfw;
+        Integral<T, Gauss, PrimalWavelet, PrimalSpline> integral_wsf, dd_integral_wsf;
+        Integral<T, Gauss, PrimalWavelet, PrimalWavelet> integral_ww, dd_integral_ww;
+            
+    public:
+        HelmholtzOperator1D(const Basis& _basis, const T _c);
+    
+        T
+        operator()(XType xtype1, int j1, int k1, 
+                   XType xtype2, int j2, int k2) const;
+    
+        T
+        operator()(T time, 
+                   XType xtype1, int j1, int k1, 
+                   XType xtype2, int j2, int k2) const
+        {
+            return operator()(xtype1, j1, k1, xtype2, j2, k2);
+        }
+};
+    
+    
+} // namespace lawa
+
+#include <lawa/operators/helmholtzoperator1d.tcc>
+
+#endif // LAWA_PARABOLIC_HELMHOLTZOPERATOR1D_H
