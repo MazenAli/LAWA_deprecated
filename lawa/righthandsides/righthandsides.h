de2272d0425df0cb42bd26e85982a2f5dd2fd04a
skestler <sebastian.kestler@uni-ulm.de>
2010-09-14 18:24:20 +0200

    Minor bug fix: removed header files for which libsparsegrid (not under git control yet) is required.

--- lawa/righthandsides/righthandsides.h
+++ lawa/righthandsides/righthandsides.h
@@ -1,11 +1,11 @@
 #include <lawa/righthandsides/function2d.h>
 #include <lawa/righthandsides/homogeneousrhs.h>
-#include <lawa/righthandsides/integrals2d.h>
+//#include <lawa/righthandsides/integrals2d.h>
 #include <lawa/righthandsides/rhs.h>
 #include <lawa/righthandsides/rhswithpeaks1d.h>
 #include <lawa/righthandsides/separablefunction2d.h>
 #include <lawa/righthandsides/separablerhs2d.h>
 #include <lawa/righthandsides/separablerhswithpeaks2d.h>
-#include <lawa/righthandsides/smoothrhswithalignedsing2d.h>
+//#include <lawa/righthandsides/smoothrhswithalignedsing2d.h>
 #include <lawa/righthandsides/sumofrhsintegrals2d.h>
 #include <lawa/righthandsides/timedepseparablerhs1d.h>
