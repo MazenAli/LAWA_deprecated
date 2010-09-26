02af26e1c1eb01ed6f83e5950daad52912677822
skestler <sebastian.kestler@uni-ulm.de>
2010-09-26 19:30:19 +0200

    Minor bug fix: removed include of erased file.

--- lawa/operators/operators.h
+++ lawa/operators/operators.h
@@ -1,6 +1,5 @@
 #include <lawa/operators/helmholtzoperator1d.h>
 #include <lawa/operators/helmholtzoperator2d.h>
-#include <lawa/operators/pdeoperator2d.h>
 #include <lawa/operators/preconditioner.h>
 #include <lawa/operators/rieszoperator1d.h>
 #include <lawa/operators/spacetimeheatoperator1d.h>
