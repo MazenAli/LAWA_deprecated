96a3d5536138444e311d96ae86ef73c889a534f0
Kristina Steih <kristina.steih@uni-ulm.de>
2011-04-12 14:24:17 +0200

    Header: lawa/constructions/constructions.h

---
+++ lawa/constructions/constructions.h
@@ -0,0 +1,10 @@
+#include <lawa/constructions/basis.h>
+#include <lawa/constructions/basisfunction.h>
+#include <lawa/constructions/bspline.h>
+#include <lawa/constructions/interval/interval.h>
+#include <lawa/constructions/mra.h>
+#include <lawa/constructions/periodic/periodic.h>
+#include <lawa/constructions/realline/realline.h>
+#include <lawa/constructions/refinementmatrix.h>
+#include <lawa/constructions/support.h>
+#include <lawa/constructions/wavelet.h>
