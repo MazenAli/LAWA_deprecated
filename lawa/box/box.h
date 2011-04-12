01118822bfa14188c8736b4cb1fa9f380b43d1ca
Kristina Steih <kristina.steih@uni-ulm.de>
2011-04-12 12:29:32 +0200

    Restructuring folders (headers not yet adapted)

--- lawa/box/box.h
+++
@@ -1,6 +0,0 @@
-#include <lawa/box/evaluate.h>
-#include <lawa/box/diagonalscalingpreconditioner.h>
-#include <lawa/box/spacetimeerrorcalculation.h>
-#include <lawa/box/spacetimeh1norm.h>
-#include <lawa/box/sparsetensorbasis.h>
-#include <lawa/box/tensorbasis.h>
--- lawa/infrastructure/infrastructure.h
+++
@@ -1,3 +0,0 @@
-#include <lawa/infrastructure/problem1d.h>
-#include <lawa/infrastructure/problem2d.h>
-#include <lawa/infrastructure/uniformindex2d.h>
