41d9c9eeedc0d5403c36566ab9d2412bb225b535
Kristina Steih <kristina.steih@uni-ulm.de>
2011-04-13 09:44:10 +0200

    Adaptive Operators: created folder, moved tensormatrix and (old) identityoperator2d here
    -> adapted folder header files

---
+++ lawa/methods/adaptive/datastructures/operators/operators.h
@@ -0,0 +1,3 @@
+#include <lawa/methods/adaptive/datastructures/operators/adaptiveidentityoperator2d.h>
+#include <lawa/methods/adaptive/datastructures/operators/tensormatrix2d.h>
+#include <lawa/methods/adaptive/datastructures/operators/tensormatrix3d.h>
