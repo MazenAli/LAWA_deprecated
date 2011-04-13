e94133986f6ccc1787ed8f54a782ec0c0c1197ac
Alexander Stippler <stip@wlan238-069.wlan.uni-ulm.de>
2011-04-13 09:45:39 +0200

    new feature: compile time assertions

---
+++ lawa/aux/compiletime_assert.h
@@ -0,0 +1,6 @@
+#ifndef LAWA_AUX_COMPILETIME_ASSERT_H
+#define LAWA_AUX_COMPILETIME_ASSERT_H 1
+
+#define ct_assert(x) extern int __compiletime_assert[(int)x]
+
+#endif // LAWA_AUX_COMPILETIME_ASSERT
