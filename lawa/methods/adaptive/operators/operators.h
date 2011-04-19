8a4afdb60efb5953c858dd0a700879881cb98b46
Kristina Steih <kristina.steih@uni-ulm.de>
2011-04-19 08:42:17 +0200

    Removed Adaptive Space Time Heat Operator 2D (again) and changed operator.h

--- lawa/methods/adaptive/operators/operators.h
+++ lawa/methods/adaptive/operators/operators.h
@@ -1,2 +1,3 @@
 #include <lawa/methods/adaptive/operators/adaptiveidentityoperator2d.h>
 #include <lawa/methods/adaptive/operators/adaptivehelmholtzoperator2d.h>
+#include <lawa/methods/adaptive/operators/adaptivespacetimeheatoperator1d.h>
