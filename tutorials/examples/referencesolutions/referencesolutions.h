5166de0f4666d48c4d614562898fe3e55a5c0f4f
Kristina Steih <kristina.steih@uni-ulm.de>
2011-04-19 16:36:43 +0200

    SpaceTime Tensor Reference Solutions

--- tutorials/examples/referencesolutions/referencesolutions.h
+++ tutorials/examples/referencesolutions/referencesolutions.h
@@ -1,3 +1,4 @@
 #include <tutorials/examples/referencesolutions/pdereferencesolutions1d.h>
 #include <tutorials/examples/referencesolutions/referencesolutions2d.h>
 #include <tutorials/examples/referencesolutions/referencesolutions3d.h>
+#include <tutorials/examples/referencesolutions/spacetimetensorrefsols1d.h>
