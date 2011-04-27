003ff39eef0245d9aec38a162665039cc63df280
Kristina Steih <kristina.steih@uni-ulm.de>
2011-04-27 12:42:51 +0200

    Base Class for 2d-Operators

--- lawa/operators/operators.h
+++ lawa/operators/operators.h
@@ -1,4 +1,5 @@
 #include <lawa/operators/integraloperators/integraloperators.h>
+#include <lawa/operators/operator2d.h>
 #include <lawa/operators/pdeoperators1d/pdeoperators1d.h>
 #include <lawa/operators/pdeoperators2d/pdeoperators2d.h>
 #include <lawa/operators/pdeoperators3d/pdeoperators3d.h>
