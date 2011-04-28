cdc813f3cb8491cda597db2f6d60f0857424cff3
Kristina Steih <kristina.steih@uni-ulm.de>
2011-04-28 10:35:16 +0200

    AdaptiveOperator Base Class

---
+++ lawa/methods/adaptive/operators/adaptiveoperator2d.h
@@ -0,0 +1,20 @@
+#ifndef LAWA_METHODS_ADAPTIVE_OPERATORS_ADAPTIVEOPERATOR2D_H
+#define LAWA_METHODS_ADAPTIVE_OPERATORS_ADAPTIVEOPERATOR2D_H 1
+
+#include <lawa/methods/adaptive/datastructures/index.h>
+
+namespace lawa {
+    
+template <typename T, typename Basis>
+struct AdaptiveOperator2D {
+               
+    virtual T
+    operator()(const Index2D &row_index, const Index2D &col_index) const = 0;
+    
+    CompressionPDE2D<T, Basis> compression;
+    
+};
+    
+} // namespace lawa
+
+#endif // LAWA_METHODS_ADAPTIVE_OPERATORS_ADAPTIVEOPERATOR2D_H
--- lawa/methods/adaptive/operators/operators.h
+++ lawa/methods/adaptive/operators/operators.h
@@ -1,3 +1,4 @@
+#include <lawa/methods/adaptive/operators/adaptiveoperator2d.h>
 #include <lawa/methods/adaptive/operators/adaptiveidentityoperator2d.h>
 #include <lawa/methods/adaptive/operators/adaptivehelmholtzoperator2d.h>
 #include <lawa/methods/adaptive/operators/adaptivespacetimepdeoperator1d.h>
