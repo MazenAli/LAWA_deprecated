ff83fbfb11e393cb4ab1e3fdb57b0ae702f43f17
skestler <sebastian.kestler@uni-ulm.de>
2011-05-14 18:17:58 +0200

    Added weighted helmholtz operator.

--- lawa/operators/pdeoperators1d/pdeoperators1d.h
+++ lawa/operators/pdeoperators1d/pdeoperators1d.h
@@ -3,6 +3,7 @@
 #include <lawa/operators/pdeoperators1d/identityoperator1d.h>
 #include <lawa/operators/pdeoperators1d/laplaceoperator1d.h>
 #include <lawa/operators/pdeoperators1d/pdeoperator1d.h>
+#include <lawa/operators/pdeoperators1d/weightedhelmholtzoperator1d.h>
 #include <lawa/operators/pdeoperators1d/weightedidentityoperator1d.h>
 #include <lawa/operators/pdeoperators1d/weightedlaplaceoperator1d.h>
 #include <lawa/operators/pdeoperators1d/weightedpdeoperator1d.h>
