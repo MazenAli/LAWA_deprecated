2cb4ad57c96b6db66fc85448f732346d089b8aa5
Kristina Steih <ksteih@creagdhubh.mathematik.uni-ulm.de>
2011-09-16 11:13:34 +0200

    Tutorials / RB Examples: Reorganization

--- tutorials/rb_examples/indexset-based/ex1/online.cpp
+++ tutorials/rb_examples/indexset-based/ex1/online.cpp
@@ -9,76 +9,10 @@
 
 #include <iostream>
 #include <lawa/lawa.h>
-#include <lawa/methods/rb/postprocessing/plotting.h>
+#include "problem.h"
 
 using namespace std;
 using namespace lawa;
-
-typedef double T;
-
-
-// Basis definitions
-typedef Basis<T,Primal,Interval,Dijkema>                    IntervalBasis;
-typedef TensorBasis2D<Adaptive,IntervalBasis,IntervalBasis> Basis2D;
-
-// Operator Definitions
-typedef WeightedHelmholtzOperator2D<T, Basis2D>                            WeightedLaplaceOp2D;
-typedef H1NormPreconditioner2D<T, Basis2D>                                 DiagPrec2D;
-typedef NoPreconditioner<T, Index2D>                                       NoPrec2D;
-typedef WeightedAdaptiveHelmholtzOperator2D<T, Basis2D, NoPrec2D>          WeightedAdaptHHOp2D;
-typedef AdaptiveHelmholtzOperator2D<T, Basis2D, NoPrec2D>                  AdaptHHOp2D;
-typedef RHS<T,Index2D, SeparableRHS2D<T, Basis2D>, NoPrec2D>               AdaptRHS;
-
-// Algorithm Definition
-    // Class for calling the truth solver for snapshot calculations
-typedef CompressionWeightedPDE2D<T, Basis2D>                              Compression;
-typedef IndexsetTruthSolver<T, Basis2D, DiagPrec2D, 
-                            Index2D, Compression>                         IndexsetSolver;
-    // Class containing all \calN-dependent data and functions
-typedef AdaptiveRBTruth2D<T, Basis2D, DiagPrec2D, 
-                            IndexsetSolver, Compression>                  RBTruth;
-    // Class containing only N-dependent data and functions
-typedef RBModel2D<T, RBTruth>                                             RBModel;
-
-// Data type definitions
-typedef Coefficients<Lexicographical,T,Index2D>                       Coeffs;
-typedef flens::DenseVector<flens::Array<T> >                          DenseVectorT;
-typedef flens::GeMatrix<flens::FullStorage<T, cxxblas::ColMajor> >    FullColMatrixT;
-
-
-/* Example: Thermal block  
- *
- *          - theta * u_xx = f on (0,1)^2
- *         u(x,0) = u(x,1) = 0
- *
- *        Here: theta = theta_1 on (0, 0.5) x (0,1)
- *              theta = theta_2 on (0,5, 1) x (0,1)
- *                  f = 1       on (0.4, 0.6) x (0.45, 0.55)
- */
-
-T
-theta_a_1(const std::vector<T>& mu)
-{
-    return mu[0];
-}
-
-T
-theta_a_2(const std::vector<T>&)
-{
-    return 1;
-}
-
-T
-theta_f_1(const std::vector<T>&)
-{
-    return 1.;
-}
-
-T
-plot_dummy_fct(T x, T y)
-{
-    return 0;
-}
 
 int main(int argc, char* argv[]) {
 
@@ -103,7 +37,6 @@ int main(int argc, char* argv[]) {
     basis_y.enforceBoundaryCondition<DirichletBC>();
     Basis2D         basis2d(basis_x, basis_y);
     
-    DiagPrec2D             prec(basis2d);
     NoPrec2D               noprec;
 
     /* Model Initialization */
--- tutorials/rb_examples/indexset-based/ex1/test.cpp
+++ tutorials/rb_examples/indexset-based/ex1/test.cpp
@@ -9,104 +9,10 @@
 
 #include <iostream>
 #include <lawa/lawa.h>
-#include <lawa/methods/rb/postprocessing/plotting.h>
+#include "problem.h"
 
 using namespace std;
 using namespace lawa;
-
-typedef double T;
-
-
-// Basis definitions
-typedef Basis<T,Primal,Interval,Dijkema>                    IntervalBasis;
-typedef TensorBasis2D<Adaptive,IntervalBasis,IntervalBasis> Basis2D;
-
-// Operator Definitions
-typedef WeightedHelmholtzOperator2D<T, Basis2D>                           WeightedLaplaceOp2D;
-typedef H1NormPreconditioner2D<T, Basis2D>                                DiagPrec2D;
-typedef NoPreconditioner<T, Index2D>                                      NoPrec2D;
-typedef WeightedAdaptiveHelmholtzOperator2D<T, Basis2D, NoPrec2D>         WeightedAdaptHHOp2D;
-typedef AdaptiveHelmholtzOperator2D<T, Basis2D, NoPrec2D>                 AdaptHHOp2D;
-typedef RHS<T,Index2D, SeparableRHS2D<T, Basis2D>, NoPrec2D>              AdaptRHS;
-
-// Algorithm Definition
-// Class for calling the truth solver for snapshot calculations
-typedef CompressionWeightedPDE2D<T, Basis2D>                                    Compression;
-typedef IndexsetTruthSolver<T, Basis2D, DiagPrec2D, Index2D, Compression>       IndexsetSolver;
-// Class containing all \calN-dependent data and functions
-typedef AdaptiveRBTruth2D<T, Basis2D, DiagPrec2D,IndexsetSolver, Compression>   RBTruth;
-// Class containing only N-dependent data and functions
-typedef RBModel2D<T, RBTruth>                                                   RBModel;
-
-// Data type definitions
-typedef Coefficients<Lexicographical,T,Index2D>                       Coeffs;
-typedef flens::DenseVector<flens::Array<T> >                          DenseVectorT;
-typedef flens::GeMatrix<flens::FullStorage<T, cxxblas::ColMajor> >    FullColMatrixT;
-
-
-/* Example: Thermal block  
- *
- *          - theta * u_xx = f on (0,1)^2
- *         u(x,0) = u(x,1) = 0
- *
- *        Here: theta = theta_1 on (0, 0.5) x (0,1)
- *              theta = theta_2 on (0,5, 1) x (0,1)
- *                  f = 1       on (0.4, 0.6) x (0.45, 0.55)
- */
- 
-T
-weight_Omega_x_1(T x)
-{
-    return (x <= 0.5) ? 1 : 0;
-}
-
-T
-weight_Omega_x_2(T x)
-{
-    return (x >= 0.5) ? 1 : 0;
-}
-
-T
-weight_Omega_y(T y)
-{
-    return 1;
-}
-
-T
-theta_a_1(const std::vector<T>& mu)
-{
-    return mu[0];
-}
-
-T
-theta_a_2(const std::vector<T>&)
-{
-    return 1;
-}
-
-T
-weight_Forcing_x(T x)
-{
-    return (x >= 0.4) && (x <= 0.6) ? 1 : 0;    
-}
-
-T
-weight_Forcing_y(T y)
-{
-    return (y >= 0.45) && (y <= 0.55) ? 1 : 0;    
-}
-
-T
-theta_f_1(const std::vector<T>&)
-{
-    return 1.;
-}
-
-T
-plot_dummy_fct(T x, T y)
-{
-    return 0;
-}
 
 int main(int argc, char* argv[]) {
 
@@ -207,14 +113,11 @@ int main(int argc, char* argv[]) {
     
     // Construct test set
     std::vector<std::vector<T> > Xi_test;
-    T test_min = 0.13;
-    T test_max = 1.96;
-    int n_test = 29;
-    T h_test = (test_max - test_min) / (n_test-1);
+
     cout << "Testset: " << endl;
     for(int i = 0; i < n_test; ++i){
       vector<T> test_param;
-      test_param.push_back(test_min + h_test * i);
+      test_param.push_back(test_min + test_h * i);
       Xi_test.push_back(test_param);
       cout << Xi_test[i][0] << endl;
     }
